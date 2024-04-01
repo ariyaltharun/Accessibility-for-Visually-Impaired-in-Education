35193e6f784697df62710808f607bbbd3d239214
M Nanditha Prabhu <nanditha7prabhu@gmail.com>
2024-04-02 02:12:34 +0530

    Made a conversational interface for 'Record Lectures' ONLY - Made a Chatbot like interface with streamlit - Whatever user speaks will be added as users prompt - The words from the application conveys whill be added as the assistant reply - As of now, the code is complete only for 'record lectures' action - It must be done for other actions as well - The 'listen' GIF is added. But, it is always running. If possible we must find a way to stop it too.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from time import sleep
 import speech_recognition as sr
 import streamlit as st
+import streamlit.components.v1 as components
+import base64
 
 from utils import listen, speak
 from Actions.LectureManager import record_lectures, replay_lecture
@@ -19,6 +21,8 @@ system_text: str = None
 button_click: bool = False
 exit: bool = False
 
+# def response_generator():
+
 
 def actions(user_text) -> None:
     global exit
@@ -30,9 +34,20 @@ def actions(user_text) -> None:
         # Start recording the lectures by activating certain devices e.g,. microphone
         # Give a name to the lecture
         # Store in a database
-        speak("Started Recording Lectures")
-        speak("What would you name this lecture?")
+
+        with st.chat_message("assistant"):
+            st.write("Started Recording Lectures!")
+            speak("Started Recording Lectures")
+            st.write("What would you name this lecture?")
+            speak("What would you name this lecture?")
+
         filename = listen()
+        with st.chat_message("user"):
+            st.write(filename)
+        st.session_state.messages.append({"role": "user", "content": filename})
+
+        with st.chat_message("assistant"):
+            st.markdown(f"{filename} name is cool")
         speak(f"{filename} name is cool")
         filename = filename.replace(" ", "_") + ".wav"
         record_lectures(filename)
@@ -47,7 +62,7 @@ def actions(user_text) -> None:
         user_response: str = listen()
         replay_lecture(user_response.replace(" ", "_"))
         pass
-    elif "act as scribe" in user_text.lower():
+    elif "scribe" in user_text.lower():
         # TODO: scribe function
         speak("Started Acting as a scribe")
         sleep(1)
@@ -63,13 +78,60 @@ def main() -> None:
     global exit
     global button_click
 
-    button = st.button(label="On Microphone")
+    st.title("Education for Visually Impaired")
 
-    if button:
-        speak("How can I help you?")
-        user_text = listen()
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("5"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            response = st.write("How can I help you?")
+            speak("How can I help you?")
+            # gif from local file
+            file_ = open("listen.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
+
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" width=200 alt="listening gif">',
+                unsafe_allow_html=True,
+            )
+            st.write("Listening...")
+            user_text = listen()
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        with st.chat_message("user"):
+            st.markdown(user_text)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": user_text})
+
+        # with st.chat_message("assistant"):
         system_text = actions(user_text)
+        st.write(system_text)
         speak(system_text)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": system_text})
+
+    # button = st.button(label="On Microphone")
+
+    # if button:
+        
+        
+        
 
 
 if __name__ == "__main__":
